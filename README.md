0e2ba69316f31495529511cd6fca714e52e1f459
Bendy Zhang <zb@bndy.net>
2019-04-27 21:42:12 +0800

    add dark theme

---
+++ README.md
@@ -0,0 +1,14 @@
+# @bndynet/dialog-themes
+
+[DEMO](https://bndynet.github.io/dialog-themes/)
+
+Themes for [@bndynet/dialog](https://github.com/bndynet/dialog)
+
+## Usage
+
+```javascript
+dialog.setup({
+    theme: 'dialog-dark',
+    //...
+});
+```
---
+++ docs/script.js
@@ -0,0 +1,27 @@
+var elThemes = document.getElementById("ddlThemes");
+for(var theme of themes) {
+    if (theme !== "default") {
+        addCssFile("../dist/" + theme + ".css", "https://unpkg.com/@bndynet/dialog-themes/dist/" + theme + ".css");
+    }
+    var option = document.createElement("option");
+    option.innerText = theme;
+    option.value = (theme === "default" ? "" : theme);
+    elThemes.appendChild(option);
+}
+changeTheme(document.getElementById("ddlThemes"));
+
+function changeTheme(sender) {
+    var value = sender.value;
+    document.body.className = value;
+}
+function addCssFile(localPath, externalPath) {
+    var el = document.createElement("link");
+    el.setAttribute("rel", "stylesheet");
+    el.setAttribute("type", "text/css");
+    if (location.hostname === '127.0.0.1' || location.hostname === 'localhost') {
+        el.setAttribute("href", localPath);
+    } else {
+        el.setAttribute("href", externalPath);
+    }
+    document.head.appendChild(el);
+}
