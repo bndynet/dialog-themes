d98d3825618c50d2e05eb9d05ff69af6d37736bd
Bendy Zhang <zb@bndy.net>
2019-06-01 13:07:04 +0800

    docs: update README and demo

--- README.md
+++ README.md
@@ -18,10 +18,5 @@ dialog.setup({
 1. Clone this repo.
 1. `npm install` in root directory.
 1. Add new theme file in **src** folder and the file name MUST be same with **classname**.
-1. Add theme name(classname) to demo site in **index.html** file.
-
-    ```javascript
-    var themes = ["your-theme", "dialog-dark", "default"];
-    ```
-
+1. Add theme name(classname) to demo site in **./docs/config.json** file.
 1. `npm start` to open demo site.
--- docs/script.js
+++ docs/script.js
@@ -1,27 +1,40 @@
-var elThemes = document.getElementById("ddlThemes");
-for(var theme of themes) {
-    if (theme !== "default") {
-        addCssFile("../dist/" + theme + ".css", "https://unpkg.com/@bndynet/dialog-themes/dist/" + theme + ".css");
-    }
-    var option = document.createElement("option");
-    option.innerText = theme;
-    option.value = (theme === "default" ? "" : theme);
-    elThemes.appendChild(option);
-}
-changeTheme(document.getElementById("ddlThemes"));
-
 function changeTheme(sender) {
     var value = sender.value;
     document.body.className = value;
 }
-function addCssFile(localPath, externalPath) {
+
+function addCssFile(filename) {
     var el = document.createElement("link");
     el.setAttribute("rel", "stylesheet");
     el.setAttribute("type", "text/css");
-    if (location.hostname === '127.0.0.1' || location.hostname === 'localhost') {
-        el.setAttribute("href", localPath);
+    if (
+        (location.hostname === "127.0.0.1" || location.hostname === "localhost")
+        && typeof themeSiteUrl === 'undefined'
+    ) {
+        el.setAttribute("href", "../dist/" + filename);
     } else {
-        el.setAttribute("href", externalPath);
+        el.setAttribute("href", "https://unpkg.com/@bndynet/dialog-themes/dist/" + filename);
     }
     document.head.appendChild(el);
 }
+
+var configFile = 'config.json';
+if (typeof themeSiteUrl !== 'undefined') {
+    configFile = themeSiteUrl + configFile;
+}
+fetch(configFile).then(function(response) {
+    return response.json();
+}).then(function(json) {
+    var elThemes = document.getElementById("ddlThemes");
+    var themes = json.themes;
+    for (var theme of themes) {
+        if (theme !== "default") {
+            addCssFile(theme + ".css");
+        }
+        var option = document.createElement("option");
+        option.innerText = theme;
+        option.value = theme === "default" ? "" : theme;
+        elThemes.appendChild(option);
+    }
+    changeTheme(document.getElementById("ddlThemes"));
+});
